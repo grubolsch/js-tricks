909e60457563ff8a94e16810383af33dd22e5b82
Koen Eelen <koen@becode.org>
2020-09-29 14:54:03 +0200

    information of first day JS course

---
+++ index.js
@@ -0,0 +1,7 @@
+var test = /** @class */ (function () {
+    function test() {
+        this.test = 'test';
+        console.log(this.test);
+    }
+    return test;
+}());
---
+++ index.ts
@@ -0,0 +1,7 @@
+class test {
+    private test : string = 'test';
+
+    constructor() {
+        console.log(this.test);
+    }
+}
